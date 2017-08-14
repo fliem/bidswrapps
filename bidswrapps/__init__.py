ea80ad4c01dc5d4fdded9162941e25e94def91e6
fliem <franz.liem@gmail.com>
2017-08-14 14:44:19 +0200

    changes update

--- bidswrapps/__init__.py
+++ bidswrapps/__init__.py
@@ -1,6 +1,7 @@
-__version__ = "0.1.11.dev"
+__version__ = "0.1.12.dev"
 
 __changelog__ ="""
+0.1.12dev fix version arg issue
 0.1.11dev added wait for nfs
 0.1.10dev fixed docker_opt bug
 0.1.9.dev added --docker_opt
