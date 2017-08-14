0ac0000dec3122c63cf4a4b00ad2161631086f1f
fliem <franz.liem@gmail.com>
2017-08-14 15:40:08 +0200

    fix changes descr

--- bidswrapps/__init__.py
+++ bidswrapps/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.1.14.dev"
 
 __changelog__ ="""
-0.1.13dev added gstat RUNNING to check logfiles
+0.1.14dev added gstat RUNNING to check logfiles
 0.1.13dev added gstat to check logfiles
 0.1.12dev fix version arg issue
 0.1.11dev added wait for nfs
