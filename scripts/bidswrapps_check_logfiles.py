ecd4da27814d3c1121c07797be5f41afdee98b8c
fliem <franz.liem@gmail.com>
2017-08-14 15:11:08 +0200

    gstat in check logfiles

--- scripts/bidswrapps_check_logfiles.py
+++ scripts/bidswrapps_check_logfiles.py
@@ -7,11 +7,12 @@ from bidswrapps.bidswrapps_echo_and_run_cmd import print_stars
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Checks bidsapps logfiles')
-    parser.add_argument('logfiles_dir',nargs='*',default=os.getcwd(),
+    parser.add_argument('logfiles_dir', default=os.getcwd(),
                         help='The directory with the bidsapps logfile folders. Default: current directory')
     args = parser.parse_args()
 
-    os.chdir(os.path.abspath(args.logfiles_dir))
+    log_dir = os.path.abspath(args.logfiles_dir)
+    os.chdir(log_dir)
 
     files = glob("*/*.log")
     good = []
@@ -37,3 +38,17 @@ if __name__ == "__main__":
 
     print("%s ok jobs" % len(good))
     print("%s bad jobs" % len(bad))
+
+    info_file = os.path.join(log_dir, ".session_path")
+    if os.path.exists(info_file):
+        with open(info_file) as fi:
+            session_path = fi.read()
+        print("Session path is {}".format(session_path))
+        cmd = "gstat -s {session_path} -v -u -l failed".format(session_path)
+        print("Checking for failed jobs {}\n Check for jobs that have something other that TERMINATED... at "
+              "Info".format(cmd))
+
+        os.system(cmd)
+
+    else:
+        print("Not checking session since {} not found".format(info_file))
